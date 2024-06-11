f93915855c3dff1b6a60f892590800ac1499d127
Samarth Raipuriya <71208433+samarth36483@users.noreply.github.com>
2024-06-11 12:00:37 +0530

    Updated contact details

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -1,10 +1,10 @@
 export const contact = {
-  desc: "MiningValleys wishes to build a friendly business relationship with all customers from the world around.",
+  desc: "Kanak Shree Minerals wishes to build a friendly business relationship with all customers from the world around.",
   formDesc:
     "Need to request quotes about our mineral products like raw mica flake, colored mica flake, and so on? Or just need help? Use the form to reach out and we will be in touch with you as quickly as possible!",
   contacts: [
     { title: "Address", text: "Rajasthan, India", icon: "location" },
-    { title: "Email", text: "kanakshreexports@gmail.com", icon: "email" },
-    { title: "WhatsApp", text: "+91-0123456789", icon: "whatsApp" },
+    { title: "Email", text: "kanakshreem@gmail.com", icon: "email" },
+    { title: "WhatsApp", text: "+91-9414010601", icon: "whatsApp" },
   ],
 };
