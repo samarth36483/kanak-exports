edcdb39b5b76cff67000396bc0d843fd58c0d79f
Samarth Raipuriya <71208433+samarth36483@users.noreply.github.com>
2024-06-11 15:51:29 +0530

    Update app-menu.ts

--- src/data/app-menu.ts
+++ src/data/app-menu.ts
@@ -1,29 +1,44 @@
 export const appMenu = [
   {
-    title: "Mica Products",
+    title: "Mica",
     menu: [
+      { text: "About Mica", link: "/" },
+      { text: "Processing Techniques", link: "/" },
+      { text: "Market & Application", link: "/" },
       { text: "Natural Mica Flakes", link: "/" },
-      { text: "Coloured Mica Flakes", link: "/" },
-      { text: "Synthetic Mica", link: "/" },
-      { text: "Mica Sheets & Disks", link: "/" },
+      { text: "Mica Coarse", link: "/" },
+      { text: "Mica Medium", link: "/" },
+      { text: "Mica Fine", link: "/" },
+      { text: "Micro Mica Powder(Dry Ground)", link: "/" },
+      { text: "Wet Ground Mica Powder", link: "/" },
     ],
   },
   {
     title: "Quartz",
     menu: [
-      { text: "Silica sand", link: "/" },
+      { text: "About Quartz", link: "/" },
+      { text: "Quartz Lumps", link: "/" },
+      { text: "Quartz Chips/Grains", link: "/" },
     ],
   },
   {
     title: "Feldspar",
     menu: [
-      { text: "Silica sand", link: "/" },
+      { text: "About Feldspar", link: "/" },
+      { text: "Feldspar Lumps", link: "/" },
+      { text: "Feldspar Chips/Grains", link: "/" },
+      { text: "Feldspar Powder", link: "/" },
     ],
   },
   {
-    title: "About",
+    title: "Company",
     menu: [
       { text: "About Us", link: "/about" },
+      { text: "Philosophy", link: "/about" },
+      { text: "Leadership", link: "/about" },
+      { text: "Production Facility", link: "/about" },
+      { text: "Quality Control", link: "/about" },
+      { text: "No Child Labour Policy", link: "/about" },
       { text: "Contact Us", link: "/contact" },
     ],
   },
